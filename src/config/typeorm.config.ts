3fbbdd63a53916e249a266d107cbdf839f7a879e
baris.gungor@etiya.com <baris.gungor@etiya.com>
2023-05-29 13:49:43 +0300

    deploy prepare

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,14 +1,15 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Chat } from '../modules/chat';
 import { Users } from '../modules/users';
+require('dotenv').config();
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5433,
-  username: 'postgres',
-  password: 'barisgungor',
-  database: 'nest-chat',
-  entities: [Chat, Users],
-  synchronize: true,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  autoLoadEntities: true,
+  synchronize: process.env.DB_SYNC === 'true',
 };
