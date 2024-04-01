73939447e58a99e4ee3670c62a73e74007f359f5
Barış Güngör <baris.gungor@etiya.com>
2024-04-01 21:26:10 +0300

    refactor(nest-api): update on github login

--- nest-api/src/modules/users/users.service.ts
+++ nest-api/src/modules/users/users.service.ts
@@ -105,13 +105,13 @@ export class UsersService {
     const client_id = '7972603f3e53797500b5';
     const redirect_uri = 'http://localhost:3000/users/github-receive-callback';
     const state = uuid();
-    const allow_signup = false;
+    const allow_signup = false; // example, can be changed
     const clientIdEncoded = encodeURIComponent(client_id);
     const redirectUriEncoded = encodeURIComponent(redirect_uri);
     const stateEncoded = encodeURIComponent(state);
     const clientSecretEncoded = encodeURIComponent(client_secret);
     const parameters = `client_id=${clientIdEncoded}&redirect_uri=${redirectUriEncoded}&state=${stateEncoded}&allow_signup=${allow_signup}`;
-    const fUrl = `https://github.com/login/oauth/authorize?${parameters}`;
+    const loginUrl = `https://github.com/login/oauth/authorize?${parameters}`;
     const githubKey = {
       clientIdEncoded,
       redirectUriEncoded,
@@ -119,35 +119,46 @@ export class UsersService {
       state,
     };
     await this.cacheManager.set('githubKey', { ...githubKey }, 600000);
-    return fUrl;
+    return loginUrl;
   }
 
   public async githubReceiveCallback(query) {
-    const value = (await this.cacheManager.get('githubKey')) as any;
-    if (value && value.state == query.state) {
-      const codeEncoded = encodeURIComponent(query.code);
-      const parameters = `client_id=${value.clientIdEncoded}&redirect_uri=${value.redirectUriEncoded}&client_secret=${value.clientSecretEncoded}&code=${codeEncoded}`;
-      const headers = { Accept: 'application/json' };
-      const payload = {};
-      const urlAccessToken = `https://github.com/login/oauth/access_token?${parameters}`;
-      const respToken = await this.httpService.axiosRef.post(
-        urlAccessToken,
-        payload,
-        {
-          headers,
-        },
-      );
-      if (respToken && respToken.data && respToken.data.access_token) {
-        const urlGetUser = `https://api.github.com/user`;
-        const headers = {
-          Accept: 'application/json',
-          Authorization: `Bearer ${respToken.data.access_token}`,
-        };
-        const resUserInfo = await this.httpService.axiosRef.get(urlGetUser, {
-          headers,
-        });
-        return resUserInfo.data;
+    try {
+      const value = (await this.cacheManager.get('githubKey')) as any;
+      if (value && value.state == query.state) {
+        const codeEncoded = encodeURIComponent(query.code);
+        const parameters = `client_id=${value.clientIdEncoded}&redirect_uri=${value.redirectUriEncoded}&client_secret=${value.clientSecretEncoded}&code=${codeEncoded}`;
+        const headers = { Accept: 'application/json' };
+        const payload = {};
+        const urlAccessToken = `https://github.com/login/oauth/access_token?${parameters}`;
+        const respToken = await this.httpService.axiosRef.post(
+          urlAccessToken,
+          payload,
+          {
+            headers,
+          },
+        );
+        if (respToken && respToken.data && respToken.data.access_token) {
+          const urlGetUser = `https://api.github.com/user`;
+          const headers = {
+            Accept: 'application/json',
+            Authorization: `Bearer ${respToken.data.access_token}`,
+          };
+          const resUserInfo = await this.httpService.axiosRef.get(urlGetUser, {
+            headers,
+          });
+          if (resUserInfo && resUserInfo.data) {
+            return resUserInfo.data;
+          }
+        }
       }
+    } catch (error) {
+      this.logger.error('githubReceiveCallback', error);
+    } finally {
+      return {
+        status: 406,
+        message: 'There is an error occurred in sign in',
+      };
     }
   }
 }
